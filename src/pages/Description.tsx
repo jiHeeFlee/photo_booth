181f02d426b8952f4dc9046e2b3c42c9ce06b371
jihee <esder1310@naver.com>
2025-11-19 15:58:31 +0900

    style(Description.tsx) : #5 페이지 레이아웃 작업

--- src/pages/Description.tsx
+++ src/pages/Description.tsx
@@ -1,3 +1,56 @@
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+import { TITLE, DESCRIPTION, BUTTON } from "../constants/Description";
+
 export default function Description() {
-  return <p>Description</p>;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Header>{TITLE}</Header>
+      <Contents>{DESCRIPTION}</Contents>
+      <GotoPhoto onClick={() => navigate("/photo")}>{BUTTON}</GotoPhoto>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  cursor: url("/cursor.png"), pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Header = styled.h1``;
+
+const Contents = styled.span`
+  white-space: pre-line;
+`;
+
+const GotoPhoto = styled.button`
+  cursor: url("/cursor.png"), pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 360px;
+  height: 60px;
+
+  font-size: 32px;
+  font-weight: 700;
+  color: var(--gr-100);
+  background-color: var(--pr-pink);
+  outline: none;
+
+  :hover {
+    transform: rotate(-2deg);
+    transition: all 0.2s ease-in-out;
+  }
+`;
