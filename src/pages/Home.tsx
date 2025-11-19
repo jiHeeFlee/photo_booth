d9d3d0453dc2db82d7eeda42d5157ed1d513c753
jihee <esder1310@naver.com>
2025-11-19 15:58:18 +0900

    style(Home.tsx) : #5 페이지 레이아웃 작업

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,49 @@
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+import { PAGE_NAME, BUTTON } from "../constants/Home";
+
 export default function Home() {
-  return <p>Home</p>;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      {PAGE_NAME}
+      <GotoDescription onClick={() => navigate("/description")}>
+        {BUTTON}
+      </GotoDescription>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+const GotoDescription = styled.button`
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 360px;
+  height: 60px;
+
+  font-size: 32px;
+  color: var(--gr-100);
+  background-color: var(--pr-pink);
+  outline: none;
+
+  :hover {
+    transform: rotate(-2deg);
+    transition: all 0.2s ease-in-out;
+  }
+`;
