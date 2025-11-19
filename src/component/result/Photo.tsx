34a8e69418eded91c0433f9511f23a503a86dc1c
jihee <esder1310@naver.com>
2025-11-19 16:00:25 +0900

    feat(Result-components) : #5 Result 컴포넌트 구현

---
+++ src/component/result/Photo.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export default function Photo() {
+  return <Container>test</Container>;
+}
+
+const Container = styled.div`
+  width: 154.88px;
+  height: 637px;
+  background-color: #1c0303;
+`;
